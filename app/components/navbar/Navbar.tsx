db590f427dcb69cffd0b44a5eccefb40e9832240
S-dev-coder <sk7137389@gmail.com>
2023-10-30 15:43:55 +0530

    Airbnb:Categories UI Added List of Categories with icons Beach Windmills Modern Countryside Pools Islands Lake Skiing Castles Caves Camping Arctic Desert Barns Lux

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Container from '../Container';
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from './UserMenu';
+import Categories from './Categories';
 
 
 interface NavbarProps{
@@ -25,6 +26,7 @@ const Navbar: React.FC<NavbarProps> = ({
           </div>
         </Container>
       </div>
+      < Categories />
     </div>
   );
 };
