549674acad7fec02c247722d691dadeebd740c4a
S-dev-coder <sk7137389@gmail.com>
2023-10-31 22:22:53 +0530

    Airbnb: Listings(Part 3) RentModal: Added INFO section for (Airbnb my home). Counter.tsx :Increment/Decrement Counter icons for Guest Details || Room Details || Bathroom Details.

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -8,6 +8,8 @@ import CategoryInput from "../Inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../Inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../Inputs/Counter";
+import ImageUpload from "../Inputs/ImageUpload";
 // import Map from "../Map";
 
 enum STEPS {
@@ -51,6 +53,11 @@ const RentModal = () => {
 
       const category = watch('category');
       const location = watch('location');
+      const guestCount = watch("guestCount");
+      const roomCount = watch("roomCount");
+      const bathroomCount = watch("bathroomCount");
+      const imageSrc = watch("imageSrc");
+
 
       const Map = useMemo(() => dynamic(() => import('../Map'), { 
         ssr: false 
@@ -139,6 +146,52 @@ const RentModal = () => {
         );
       }
     
+    if(step=== STEPS.INFO){
+      bodyContent =(
+        <div className ="flex flex-col gap-8">
+          <Heading
+          title="Share some basics about your place"
+          subtitle ="What amenities do you have?"
+          />
+          <Counter
+            title="Guests"
+            subtitle="How many guests do you aloow?"
+            value={guestCount} 
+            onChange={(value)=> setCustomValue('guestCount' , value)} 
+            />
+            <hr />
+            <Counter
+            title="Rooms"
+            subtitle="How many rooms do you have?"
+            value={roomCount} 
+            onChange={(value)=> setCustomValue('roomCount' , value)} 
+            />
+            <hr/>
+            <Counter
+            title="Bathrooms"
+            subtitle="How many bathrooms do you have?"
+            value={bathroomCount} 
+            onChange={(value)=> setCustomValue('bathroomCount' , value)} 
+            />
+        </div>
+      )
+    }
+    
+
+    if (step === STEPS.IMAGES) {
+      bodyContent = (
+        <div className="flex flex-col gap-8">
+          <Heading
+            title="Add a photo of your place"
+            subtitle="Show guests what your place looks like!"
+          />
+          <ImageUpload
+            onChange={(value) => setCustomValue('imageSrc', value)}
+            value={imageSrc}
+          />
+        </div>
+      )
+    }
 
     return (
         <Modal
@@ -157,3 +210,4 @@ const RentModal = () => {
 }
  
 export default RentModal;
+
